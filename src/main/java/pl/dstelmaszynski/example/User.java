bdf1e04beb4a4eb7bbf84341246d0a4554abc9b6
Dominik <stelmaszynski.dominik@gmail.com>
2023-10-27 17:25:37 +0200

    BDD Mockito example

---
+++ src/main/java/pl/dstelmaszynski/example/User.java
@@ -0,0 +1,22 @@
+package pl.dstelmaszynski.example;
+
+
+import lombok.Builder;
+import lombok.Value;
+
+import java.util.Objects;
+
+@Value
+@Builder
+public class User {
+
+    String name;
+    String surname;
+    String phone;
+
+    public boolean isValid() {
+        return Objects.nonNull(name)
+                && Objects.nonNull(surname)
+                && Objects.nonNull(phone);
+    }
+}
